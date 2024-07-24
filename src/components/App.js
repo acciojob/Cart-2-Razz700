951263268401828b8a23d8c3a0bf17b944f87022
Razzakh <mrazzakh123@gmail.com>
2024-07-24 16:14:02 +0530

    old rect

--- src/components/App.js
+++ src/components/App.js
@@ -98,7 +98,9 @@ const App = () => {
                 return amount;
               });
              }}>+</button>
-             <p id={'cart-amount-'+item.id}>{amount[i].toFixed(2)}</p>
+             <p id={'cart-amount-'+item.id}>{count[i]}
+              {/* {amount[i].toFixed(2)} */}
+             </p>
              <button onClick={()=>{
                handleremoveitem(item);
                 setamount(amount=>{
