60fc2034d049e194e67062d916054d4451b4f14b
Razzakh <mrazzakh123@gmail.com>
2024-07-24 13:40:14 +0530

    nxt

--- src/components/App.js
+++ src/components/App.js
@@ -56,11 +56,11 @@ const App = () => {
   return (
    <Datacontext.Provider value={array}>
     <div id='main'>
-    <div className='navbar'>
+    <nav className='navbar'>
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
 </svg>(<span id='nav-cart-item-count'>{list.length}</span>Items)<h1>useReducer</h1>
-    </div>
+    </nav>
     {list.length>0 && <div id='cart-items-list'>
     {list.map((item,i)=>{
       return <div className='item' key={'a'+i}>
@@ -99,6 +99,19 @@ const App = () => {
               });
              }}>+</button>
              <p id={'cart-amount-'+item.id}>{amount[i]}</p>
+             <button onClick={()=>{
+               handleremoveitem(item);
+                setamount(amount=>{
+                  const arr=[...amount];
+                  delete arr[i];
+                  const arr1=[];
+                  arr.forEach((item)=>{
+                    if(item)arr1.push(item);
+                  });
+                  return arr1;
+                })
+               
+             }} id={'cart-item-remove-'+item.id}>Remove</button>
       </div>
     })}
     <p id='cart-total-amount'>{total()}</p>
