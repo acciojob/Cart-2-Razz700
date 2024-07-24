f3aff743c3c2d4ba0a86a21da67b9467d60b7f14
Razzakh <mrazzakh123@gmail.com>
2024-07-24 17:05:17 +0530

    now these

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+{/* <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
 import React, { createContext, useState } from 'react'
 import '../styles/App.css'
 import { useReducer } from 'react'
@@ -99,9 +99,7 @@ const App = () => {
                 return amount;
               });
              }}>+</button>
-             <p>
-             Amount:{amount[i].toFixed(2)}
-             </p>
+             <p>Amount:{amount[i].toFixed(2)}</p>
              <button onClick={()=>{
                handleremoveitem(item);
                 setamount(amount=>{
@@ -117,9 +115,7 @@ const App = () => {
       </div>
     })}
     <p id='cart-total-amount'>{total().toFixed(2)}</p>
-    <button onClick={()=>{
-      setlist([]);
-    }} id='clear-all-cart'>Clear All</button>
+    <button onClick={()=>setlist([])} id='clear-all-cart'>Clear All</button>
     </div>}
     {list.length==0 && <p>Cart is currently empty</p>}
     </div>
@@ -128,4 +124,126 @@ const App = () => {
   )
 }
 
-export default App
+export default App */}
+import React, { createContext, useState, useReducer } from 'react';
+import '../styles/App.css';
+import { Reducer, initialState } from './Reducer';
+
+const App = () => {
+  const [state, dispatch] = useReducer(Reducer, initialState);
+  const array = [
+    {
+      id: 1,
+      title: "Samsung Galaxy S7",
+      price: 599.99,
+      img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1583368215/phone-2_ohtt5s.png",
+      amount: 1,
+    },
+    {
+      id: 2,
+      title: "google pixel ",
+      price: 499.99,
+      img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1583371867/phone-1_gvesln.png",
+      amount: 1,
+    },
+    {
+      id: 3,
+      title: "Xiaomi Redmi Note 2",
+      price: 699.99,
+      img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1583368224/phone-3_h2s6fo.png",
+      amount: 1,
+    },
+  ];
+  const [list, setList] = useState(array);
+  const [count, setCount] = useState([1, 1, 1]);
+  const Datacontext = createContext();
+
+  const handleRemoveItem = (item) => {
+    setList((list) => list.filter((elem) => elem.title !== item.title));
+    setCount((count) => count.filter((_, index) => list[index].id !== item.id));
+  };
+
+  const total = () => {
+    return list.reduce((acc, item, index) => acc + item.price * count[index], 0);
+  };
+
+  const increment = (index) => {
+    setCount((count) => {
+      const newCount = [...count];
+      newCount[index]++;
+      return newCount;
+    });
+  };
+
+  const decrement = (index) => {
+    setCount((count) => {
+      const newCount = [...count];
+      newCount[index] = newCount[index] > 1 ? newCount[index] - 1 : 1;
+      return newCount;
+    });
+  };
+
+  return (
+    <Datacontext.Provider value={array}>
+      <div id="main">
+        <nav className="navbar">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+            />
+          </svg>
+          (<span id="nav-cart-item-count">{list.length}</span> Items)
+          <h1>useReducer</h1>
+        </nav>
+        {list.length > 0 ? (
+          <div id="cart-items-list">
+            {list.map((item, i) => (
+              <div className="item" key={item.id}>
+                <img src={item.img} alt={item.title} />
+                <h4>{item.title}</h4>
+                <p id={'cart-item-price-' + item.id}>Price: {item.price}</p>
+                <button
+                  id={'decrement-btn-' + item.id}
+                  onClick={() => decrement(i)}
+                >
+                  -
+                </button>
+                <span id={'cart-amount-' + item.id}>{count[i]}</span>
+                <button
+                  id={'increment-btn-' + item.id}
+                  onClick={() => increment(i)}
+                >
+                  +
+                </button>
+                <p>Amount: {(item.price * count[i]).toFixed(2)}</p>
+                <button
+                  onClick={() => handleRemoveItem(item)}
+                  id={'cart-item-remove-' + item.id}
+                >
+                  Remove
+                </button>
+              </div>
+            ))}
+            <p id="cart-total-amount">{total().toFixed(2)}</p>
+            <button onClick={() => setList([])} id="clear-all-cart">
+              Clear All
+            </button>
+          </div>
+        ) : (
+          <p>Cart is currently empty</p>
+        )}
+      </div>
+    </Datacontext.Provider>
+  );
+};
+
+export default App;
