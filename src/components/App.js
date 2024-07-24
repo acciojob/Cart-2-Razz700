431140eb56c3e6a725f8bb9b85c32b008450ab8c
Razzakh <mrazzakh123@gmail.com>
2024-07-24 20:49:58 +0530

    delete component

--- src/components/App.js
+++ src/components/App.js
@@ -3,33 +3,32 @@ import React, { createContext, memo, useRef, useState } from 'react'
 import '../styles/App.css'
 import { useReducer } from 'react'
 import { Reducer,initialState } from './Reducer'
-import Amount from './Amount'
 export const Datacontext=createContext();
 const App = () => {
   const [state,dispatch]=useReducer(Reducer,initialState);
-  const array=[
-    {
-      id: 1,
-      title: "Samsung Galaxy S7",
-      price: 599.99,
-      img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1583368215/phone-2_ohtt5s.png",
-      amount: 1,
-    },
-    {
-      id: 2,
-      title: "google pixel ",
-      price: 499.99,
-      img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1583371867/phone-1_gvesln.png",
-      amount: 1,
-    },
-    {
-      id: 3,
-      title: "Xiaomi Redmi Note 2",
-      price: 699.99,
-      img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1583368224/phone-3_h2s6fo.png",
-      amount: 1,
-    }
-  ];
+  // const array=[
+  //   {
+  //     id: 1,
+  //     title: "Samsung Galaxy S7",
+  //     price: 599.99,
+  //     img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1583368215/phone-2_ohtt5s.png",
+  //     amount: 1,
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "google pixel ",
+  //     price: 499.99,
+  //     img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1583371867/phone-1_gvesln.png",
+  //     amount: 1,
+  //   },
+  //   {
+  //     id: 3,
+  //     title: "Xiaomi Redmi Note 2",
+  //     price: 699.99,
+  //     img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1583368224/phone-3_h2s6fo.png",
+  //     amount: 1,
+  //   }
+  // ];
   const [list,setlist]=useState(state.arraydata);
   const handleremoveitem=(item)=>{
     setlist((list)=>list.filter((elem)=>elem.title!=item.title));
