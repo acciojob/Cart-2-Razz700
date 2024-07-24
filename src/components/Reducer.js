2fa900a724ff6eadbe587680f16c37d1d9e24d48
Razzakh <mrazzakh123@gmail.com>
2024-07-24 23:54:21 +0530

    ttgg

--- src/components/Reducer.js
+++ src/components/Reducer.js
@@ -3,7 +3,7 @@ export const initialState={
         {
           id: 1,
           title: "Samsung Galaxy S7",
-          price: 599.99,
+          price: 499.99,
           img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1583368215/phone-2_ohtt5s.png",
           amount: 1
         },
@@ -17,14 +17,14 @@ export const initialState={
         {
           id: 3,
           title: "Xiaomi Redmi Note 2",
-          price: 699.99,
+          price: 499.99,
           img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1583368224/phone-3_h2s6fo.png",
           amount: 1
         },
         {
             id: 4,
             title: "Xiaomi Note Fold 5",
-            price: 199.99,
+            price: 499.99,
             img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1583368224/phone-3_h2s6fo.png",
             amount: 1
           }
