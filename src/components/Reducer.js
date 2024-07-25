9629a7574c53ad942747f6108fdeb7ca961a600d
Razzakh <mrazzakh123@gmail.com>
2024-07-25 07:27:20 +0530

    now gg

--- src/components/Reducer.js
+++ src/components/Reducer.js
@@ -3,21 +3,21 @@ export const initialState={
         {
           id: 1,
           title: "Samsung Galaxy S7",
-          price: 499.99,
+          price: 500,
           img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1583368215/phone-2_ohtt5s.png",
           amount: 1
         },
         {
           id: 2,
           title: "google pixel ",
-          price: 499.99+100.01,
+          price: 400,
           img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1583371867/phone-1_gvesln.png",
           amount: 1
         },
         {
           id: 3,
           title: "Xiaomi Redmi Note 2",
-          price: 499.99*2,
+          price: 300,
           img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1583368224/phone-3_h2s6fo.png",
           amount: 1
         }
