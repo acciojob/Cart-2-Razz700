51d29122a61c87bfdce55ccadf264fc3f8c7801a
Razzakh <mrazzakh123@gmail.com>
2024-07-25 07:18:51 +0530

    wq

--- src/components/Reducer.js
+++ src/components/Reducer.js
@@ -10,14 +10,14 @@ export const initialState={
         {
           id: 2,
           title: "google pixel ",
-          price: 499.99*2,
+          price: 499.99,
           img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1583371867/phone-1_gvesln.png",
           amount: 1
         },
         {
           id: 3,
           title: "Xiaomi Redmi Note 2",
-          price: 499.99,
+          price: 499.99*2,
           img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1583368224/phone-3_h2s6fo.png",
           amount: 1
         }
